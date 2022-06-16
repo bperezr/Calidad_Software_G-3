0cdcd40561e7cd5262f4bbea33d4d271a11d1b9c
bperezr <bperezr@outlook.com>
2022-06-15 21:43:57 -0600

    Selenium library installed

---
+++ Project_OpenCart/src/main/java/Main.java
@@ -0,0 +1,22 @@
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebDriver;
+import org.junit.Test;
+
+public class Main {
+    
+
+    public static void main(String[] args) {    
+     System.setProperty("webdriver.chrome.driver","C:\\chromedriver_win32\\chromedriver.exe");
+        
+        ChromeDriver driver = new ChromeDriver(); 
+                     
+         driver.get("https://www.opencart.com/");
+         
+         driver.manage().window().maximize();
+             
+
+    }
+
+    
+    
+}
